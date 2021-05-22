461894e44b31de7976eee98136db3e29e4880023
byeongwan <byeongco93@gmail.com>
2021-05-22 12:01:19 +0900

    Change RegExp ID Condition, Read Watching Video data

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,10 +4,11 @@ export const home = async (req, res) => {
   const videos = await Video.find({});
   return res.render('home', { pageTitle: 'Home', videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
+  const video = await Video.findById(id);
 
-  return res.render('watch', { pageTitle: `Watching` });
+  return res.render('watch', { pageTitle: video.title, video });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
