1371d78343d19a971ae7e3597715e4e55b06ab1d
byeongwan <byeongco93@gmail.com>
2021-05-26 01:12:33 +0900

    add Video Model pre save hook

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -24,7 +24,7 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.exists({_id: id});
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render('404', { pageTitle: 'Video not found.' });
   }
@@ -32,9 +32,9 @@ export const postEdit = async (req, res) => {
     title,
     description,
     hashtags: hashtags
-    .split(',')
-    .map((word) => (word.startsWidth('#') ? word : `#${word}`));
-  })
+      .split(',')
+      .map((word) => (word.startsWidth('#') ? word : `#${word}`)),
+  });
 
   return res.redirect(`/videos/${id}`);
 };
@@ -48,13 +48,12 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(',')
-        .map((word) => (word.startsWidth('#') ? word : `#${word}`)),
+      hashtags,
     });
 
     return res.redirect('/');
   } catch (error) {
+    console.log(error);
     return res.render('upload', {
       pageTitle: 'Upload Video',
       errorMessage: error._message,
