c809dd468f36ab4d2556a5ee811b71102347a56e
byeongwan <byeongco93@gmail.com>
2021-05-27 16:10:16 +0900

    create delete, Video Model's static function

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,9 +31,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(',')
-      .map((word) => (word.startsWidth('#') ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
 
   return res.redirect(`/videos/${id}`);
@@ -48,7 +46,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
 
     return res.redirect('/');
@@ -59,4 +57,11 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  await Video.findByIdAndDelete(id);
+  return res.redirect('/');
 };
