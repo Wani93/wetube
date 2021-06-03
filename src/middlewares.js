a857a2607b03ab28838ce15d5bdab3f0e6f4ba7f
byeongwan <byeongco93@gmail.com>
2021-06-03 15:39:09 +0900

    Protector & Public Middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,8 +1,24 @@
-const localsMiddleware = (req, res, next) => {
+export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = 'Wetube';
   res.locals.loggedInUser = req.session.user;
   next();
 };
 
-export default localsMiddleware;
+// User 로그인 체크 미들웨어
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect('login');
+  }
+};
+
+// 비로그인 상태에서 접근할 수있게 하는 미들웨어
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+};
